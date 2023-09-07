2c87266fb6c9872ae36d1f7b375d989136b0e2b5
sebastian-VB <sebas99peru@gmail.com>
2023-09-07 16:42:16 -0500

    added responsive navbar

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent {
-
+export class MainPageComponent implements OnInit{
+  
   showOptions:boolean = false;
+  
+  windowSize: { width: number; height: number } = { width: window.innerWidth, height: window.innerHeight };
+  
+  ngOnInit(): void {
+    this.compareSize(this.windowSize.width);
+  }
 
   showOpt(){
     this.showOptions = !this.showOptions;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.windowSize.width = window.innerWidth;
+
+    this.compareSize(this.windowSize.width);
+    
+  }
+
+  compareSize(size: number){
+    if(size <= 750){
+      if(this.showOptions) this.showOptions = !this.showOptions;
+    }
+    else{
+      this.showOptions = true;
+    }
   }
 
 }
